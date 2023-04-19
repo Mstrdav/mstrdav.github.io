2fd334914227844f31a70fa08cd6e2b24f30401b
mstrdav <colin.geindre@gmail.com>
2023-04-19 20:52:34 +0200

    ok c pas mal la

--- js/project_selector.js
+++ js/project_selector.js
@@ -5,11 +5,8 @@ console.log("Current project: " + currentProject);
 
 const projectSelectorList = document.querySelector("#project-selector-list");
 const projectSelector = document.querySelector("#project-selector");
-let isProjectSelectorOpen = false;
 
 // project info actions buttons
-const piaPrev = document.querySelector("#pia-prev");
-const piaNext = document.querySelector("#pia-next");
 const piaGh = document.querySelector("#pia-gh");
 const piaLive = document.querySelector("#pia-live");
 
@@ -19,27 +16,8 @@ document.querySelectorAll(".project-selector-item").forEach((item) => {
   item.addEventListener("click", (e) => {
     e.preventDefault();
     if (item.getAttribute("data-project") != currentProject) {
-      if (isProjectSelectorOpen) {
-        // close the project selector
-        projectSelector.style.top = "calc(100% - 200px)";
-        isProjectSelectorOpen = false;
-      }
-
       // switch the project
       switchProject(item);
-    } else {
-      // scroll the project view to the top
-      if (isProjectSelectorOpen) {
-        console.log("Close project selector");
-        // close the project selector
-        projectSelector.style.top = "calc(100% - 200px)";
-        isProjectSelectorOpen = false;
-      } else {
-        // open the project selector
-        console.log("Open project selector");
-        projectSelector.style.top = "120px";
-        isProjectSelectorOpen = true;
-      }
     }
   });
 });
@@ -65,8 +43,11 @@ const switchProject = (item) => {
   // compute the new position of the project selector
   let newID = parseInt(item.getAttribute("data-project").split("-")[1]);
 
-  // we translate the project selector by the difference between the two IDs * 290px
-  let translate = (1 - newID) * 290;
+  // we translate the project selector by the difference between the two IDs * the size of the project selector item
+  // get the size of the project selector item
+  let itemSize = item.getBoundingClientRect().width;
+  console.log("Item size: " + itemSize);
+  let translate = (1 - newID) * itemSize;
   projectSelectorList.style.transform = "translateX(" + translate + "px)";
   console.log("Translate: " + translate);
 
@@ -78,23 +59,59 @@ const switchProject = (item) => {
   window.location.hash = item.getAttribute("data-project");
 
   // update the project info
-  piaPrev.href = "#project-" + newID;
-  piaPrev.onclick = function () {
-    console.log("Clicked prev");
-    switchProject(document.querySelector("#psi-" + (newID - 1)));
-    return true;
-  };
-  piaPrev.style.display = "inline";
-  piaNext.href = "#project-" + newID;
-  piaNext.onclick = function () {
-    console.log("Clicked next");
-    switchProject(document.querySelector("#psi-" + (newID + 1)));
-    return true;
-  };
-  piaNext.style.display = "inline";
-  if (newID == 1) {
-    piaPrev.style.display = "none";
-  } else if (newID == NB_PROJECTS) {
-    piaNext.style.display = "none";
-  }
+  // piaPrev.href = "#project-" + newID;
+  // piaPrev.onclick = function () {
+  //   console.log("Clicked prev");
+  //   switchProject(document.querySelector("#psi-" + (newID - 1)));
+  //   return true;
+  // };
+  // piaPrev.style.display = "inline";
+  // piaNext.href = "#project-" + newID;
+  // piaNext.onclick = function () {
+  //   console.log("Clicked next");
+  //   switchProject(document.querySelector("#psi-" + (newID + 1)));
+  //   return true;
+  // };
+  // piaNext.style.display = "inline";
+  // if (newID == 1) {
+  //   piaPrev.style.display = "none";
+  // } else if (newID == NB_PROJECTS) {
+  //   piaNext.style.display = "none";
+  // }
 };
+
+/* 
+  pialive
+  on click, translate the project selector and the header out of the screen
+  move the project info to the bottom
+  expand the canvas
+*/
+
+piaLive.addEventListener("click", (e) => {
+  e.preventDefault();
+  let projectInfo = document.querySelector("#project-info");
+  let projectInfoPos = projectInfo.getBoundingClientRect();
+  // hide the project selector
+  projectSelector.style.transform = "translateY(100%)";
+  // hide the header
+  document.querySelector("header").style.transform = "translateY(-100%)";
+  // move the project info to the bottom
+  // make it disappear
+  projectInfo.style.opacity = "0";
+
+  setTimeout(() => {
+    projectInfo.style.position = "fixed";
+    projectInfo.style.bottom = "0px";
+    projectInfo.style.paddingLeft = "40px";
+    // make it reappear
+    projectInfo.style.opacity = "1";
+  }, 400);
+
+  // expand the canvas
+  let projectDiv = document.querySelector("#project-div");
+  projectDiv.style.left = "0px";
+  projectDiv.style.width = "100%";
+  projectDiv.style.height = "calc(100%)";
+  projectDiv.style.top = "0px";
+  projectDiv.style.borderRadius = "0px";
+});
